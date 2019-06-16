cf83cb1270fabb8d8613dda5ddf21b483e679dee
LegDK <maltsev.ru95@mail.ru>
2019-06-16 19:31:34 +0300

    abstraction service

--- pet-clinic-data/src/main/java/ru/maltsev/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ru/maltsev/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package ru.maltsev.petclinic.services;
 
 import ru.maltsev.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
+    
 }
--- pet-clinic-data/src/main/java/ru/maltsev/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/ru/maltsev/petclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package ru.maltsev.petclinic.services;
 
-import ru.maltsev.petclinic.model.Owner;
 import ru.maltsev.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Owner owner);
-
-    Set<Vet> findAll();
 }
