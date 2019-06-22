f1be99589ea19ecde443c1a80790aa23faf4c249
LegDK <maltsev.ru95@mail.ru>
2019-06-22 22:31:42 +0300

    adding pets contacts info into bootstrap

--- pet-clinic-web/src/main/java/ru/maltsev/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ru/maltsev/petclinic/bootstrap/DataLoader.java
@@ -1,38 +1,74 @@
 package ru.maltsev.petclinic.bootstrap;
 
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ru.maltsev.petclinic.model.Owner;
+import ru.maltsev.petclinic.model.Pet;
+import ru.maltsev.petclinic.model.PetType;
 import ru.maltsev.petclinic.model.Vet;
 import ru.maltsev.petclinic.services.OwnerService;
+import ru.maltsev.petclinic.services.PetTypeService;
 import ru.maltsev.petclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCat = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("ul Pshkina dom kolotushkina");
+        owner1.setCity("Moscow");
+        owner1.setTelephone("23123131");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDog);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Vasya");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("ul Pshkina dom kolotushkina2");
+        owner2.setCity("Moscow2");
+        owner2.setTelephone("231231312");
+
+        Pet fionasPet = new Pet();
+        fionasPet.setPetType(savedCat);
+        fionasPet.setOwner(owner2);
+        fionasPet.setBirthDate(LocalDate.now());
+        fionasPet.setName("Valera");
+        owner2.getPets().add(fionasPet);
 
         ownerService.save(owner2);
 
