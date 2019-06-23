76d39b36c55890312b81e120ca10da13e3e60576
LegDK <maltsev.ru95@mail.ru>
2019-06-23 23:01:32 +0300

    updating entities

--- pet-clinic-data/src/main/java/ru/maltsev/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ru/maltsev/petclinic/model/Person.java
@@ -1,8 +1,14 @@
 package ru.maltsev.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "second_name")
     private String lastName;
 
     public String getFirstName() {
